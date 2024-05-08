ff31562b55261ec7a4e9b3b3ee83b1263687e44b
Stopa <sikhuliso@gmail.com>
2024-05-08 12:18:23 +0200

    os module

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -3,3 +3,6 @@ const os = require('os')
 //info about current user
 const user = os.userInfo()
 console.log(user)
+
+//method returns system uotime in secs
+console.log('System uptime is : ', os.uptime(),' seconds')
