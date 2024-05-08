4385f702d888e6750dff457984cb03b536d310a5
Stopa <sikhuliso@gmail.com>
2024-05-08 23:52:42 +0200

    fs asnc

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -5,5 +5,22 @@ readFile('./content/first.txt','utf8',(err,result)=>{
         console.log(err)
         return
     }
-    console.log(result)
-})
+    const first = result
+    readFile('./content/second.txt','utf8',(err,result)=>{
+        if(err){
+            console.log(err)
+            return
+        }
+        const second = result
+        appendFile('./content/file-sync.txt',' Appended Data',(err,result)=>{
+            if(err){
+                console.log(err)
+                return
+            }
+            console.log(result)
+        })
+        
+    })
+}
+
+)
