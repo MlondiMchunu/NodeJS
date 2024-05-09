77571f7816a8f9832e206ec8c55d735f160fc93a
Stopa <mlondiemchunu1@gmail.com>
2024-05-09 20:10:04 +0200

    event loop2 commit

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,6 +1,8 @@
-const http = require('http')
-const fs = require('fs')
+//const http = require('http')
+//const fs = require('fs')
 
+//const _ = require('lodash')
+/*
 const server = http.createServer((req,res)=>{
    //res.write('Welcome Mlondi') 
    //res.end()
@@ -19,3 +21,32 @@ const server = http.createServer((req,res)=>{
    res.end('<h1>Oops!!</h1> <p>Page not available</>')
 }})
 server.listen(5000)
+*/
+
+//const items = [1,[2,[3,[4]]]]
+//const newItems = _.flattenDeep(items)
+//console.log(newItems)
+//console.log('hello World')
+
+/*const {readFile} = require('fs')
+
+console.log('started a first task')
+
+//check file path
+
+readFile('./content/first.txt','utf8',(err,result)=>{
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log(result)
+    console.log('completed first task')
+})
+
+console.log('starting next task')
+*/
+console.log('first')
+setTimeout(()=>{
+    console.log('second')
+},4000)
+console.log('third')
