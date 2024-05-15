f1a34908139ae1f243b9454b644b6781c8fa7cb3
Stopa <mlondiemchunu1@gmail.com>
2024-05-15 22:00:47 +0200

    changed Switch to Routes

--- mlondi-reviews/frontend/src/App.js
+++ mlondi-reviews/frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 //page & layout imports
 import Homepage from "./pages/Homepage";
@@ -8,9 +8,22 @@ import SiteHeader from "./components/SiteHeader";
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      
+      <SiteHeader/>
+      <Routes>
+        <Route exact path="/" element={<Homepage/>}>
+          {/*<Homepage/>*/}
+        </Route>
+        <Route path="/details/:id" element={<ReviewDetails/>}>
+          {/*<ReviewDetails/>*/}
+        </Route>
+        <Route path="/category/:id" element={<Category/>}>
+          {/*<Category/>*/}
+        </Route>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
