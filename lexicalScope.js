a6cee5b7bbe31260e6731c0ec41e59521a8feb89
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-04-07 11:50:41 +0200

    commit lexical scope

---
+++ lexicalScope.js
@@ -0,0 +1,12 @@
+this.scope = "lexical scope";
+
+const scopeOf = {
+    scope: "defining scope",
+
+    traditional: function () {
+        return this.scope;
+    },
+    arrow: () => {
+        return this.scope;
+    }
+}
