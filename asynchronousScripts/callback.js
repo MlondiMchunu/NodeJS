1be9979007391d33fc7c70769fadf71424a77ae6
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-04-08 11:18:14 +0200

    commit traditional callback

---
+++ asynchronousScripts/callback.js
@@ -0,0 +1,9 @@
+import fs from "fs"
+
+const callback = (err, data) => {
+    if (err) {
+        return console.log("error")
+    } console.log(`File content ${data}`);
+};
+
+fs.readFile("file.txt", callback);
