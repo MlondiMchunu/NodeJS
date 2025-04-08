aa59c4f1ac849ad61ab791595ec318bd2a0ae0a0
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-04-08 11:39:09 +0200

    refactor callback file

--- asynchronousScripts/callback.js
+++ asynchronousScripts/callback.js
@@ -1,9 +1,18 @@
 import fs from "fs"
 
-const callback = (err, data) => {
+console.log("before callback");
+/*const callback = (err, data) => {
     if (err) {
         return console.log("error")
-    } console.log(`File content ${data}`);
-};
+    } console.log(`File content\n------------- \n${data}`);
+};*/
 
-fs.readFile("file.txt", callback);
+//fs.readFile("file.txt", callback);
+
+fs.readFile("file.txt", (err, data) => {
+    if (err) {
+        return console.log("error");
+    }
+    console.log(`File content\n------------- \n${data}`);
+})
+console.log("after callback")
