7efb66551dced4e5a8c344c996e4afedf6541d77
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-04-10 15:08:41 +0200

    commit array spreading

--- arrays/disperse/spread.js
+++ arrays/disperse/spread.js
@@ -1,3 +1,5 @@
+/*Object spread*/
+
 let object = {
     fruit: "apple",
     color: "green"
@@ -8,3 +10,14 @@ console.log(`fruit: ${fruit}, color: ${color}`)
 
 color = "red";
 console.log(`object.color: ${object.color}, color: ${color}`);
+
+/*Array spread*/
+
+let original = [1, 2, 3];
+let cloned = [...original];
+
+cloned[0] = "one";
+cloned[1] = "two";
+cloned[2] = "three";
+
+console.log(`original: ${original}, cloned: ${cloned}`);
