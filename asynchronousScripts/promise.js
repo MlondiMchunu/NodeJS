a02e2405a6d3a477b29adaf89472281dfbc570ba
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-04-10 10:12:29 +0200

    commit print statements to demonstrate promise

--- asynchronousScripts/promise.js
+++ asynchronousScripts/promise.js
@@ -1,9 +1,13 @@
+console.log("before function")
 function fetchData(url) {
-    fetch(url)
+    console.log("inside function 1")
+    fetch(url)//fetch api is promisebased by default
         .then((response) => response.json())
         .then((json) => console.log(json))
         .catch((error) => {
             console.error(`Error : ${error}`);
         });
+        console.log("inside function2")
 }
-fetchData("https://www.usermodernfullstack.dev/api/v1/users");
+fetchData("https://projects.propublica.org/nonprofits/api/v2/search.json?order=revenue&sort_order=desc");
+console.log("after function")
