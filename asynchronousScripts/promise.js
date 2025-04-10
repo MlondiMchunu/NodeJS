9a82ea3bffd51fb469f7c83a510b96dac3504889
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-04-10 11:41:49 +0200

    latest commit

--- asynchronousScripts/promise.js
+++ asynchronousScripts/promise.js
@@ -4,11 +4,12 @@ function fetchData(url) {
     fetch(url)//fetch api is promise based by default
         .then((response) => response.json()) //then called when promise is resolved
         .then((json) => console.log(json))
-        .then((fulfilled)=> console.log("promise fulfilled"))
+        .then((fulfilled) => console.log("promise fulfilled ", fulfilled))
         .catch((error) => { //catch called when promise is rejected
             console.error(`Error : ${error}`);
         });
-        console.log("inside function2")
+    console.log("inside function2")
 }
+console.log("after function 1")
 fetchData("https://projects.propublica.org/nonprofits/api/v2/search.json?order=revenue&sort_order=desc_limit=2");
-console.log("after function")
+console.log("after function 2")
