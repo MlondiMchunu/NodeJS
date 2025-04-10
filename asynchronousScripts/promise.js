7009d337949c5a824a48d3529a1f64b8e8626b2e
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-04-10 10:51:27 +0200

    testing fulfilled state

--- asynchronousScripts/promise.js
+++ asynchronousScripts/promise.js
@@ -1,13 +1,14 @@
 console.log("before function")
 function fetchData(url) {
     console.log("inside function 1")
-    fetch(url)//fetch api is promisebased by default
-        .then((response) => response.json())
+    fetch(url)//fetch api is promise based by default
+        .then((response) => response.json()) //then called when promise is resolved
         .then((json) => console.log(json))
-        .catch((error) => {
+        .then((fulfilled)=> console.log("promise fulfilled"))
+        .catch((error) => { //catch called when promise is rejected
             console.error(`Error : ${error}`);
         });
         console.log("inside function2")
 }
-fetchData("https://projects.propublica.org/nonprofits/api/v2/search.json?order=revenue&sort_order=desc");
+fetchData("https://projects.propublica.org/nonprofits/api/v2/search.json?order=revenue&sort_order=desc_limit=2");
 console.log("after function")
