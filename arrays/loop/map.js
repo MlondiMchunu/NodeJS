add7ecfaef9c2fbd38db658d6b7f219645191ba8
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-04-11 12:05:35 +0200

    routinely commit

---
+++ arrays/loop/map.js
@@ -0,0 +1,4 @@
+const original = [1, 2, 3, 4, 5, 6, 7, 8,9];
+const multiplied = original.map((item)=> item * 100);
+console.log(`original array: ${original}`);
+console.log(`multiplied array: ${multiplied}`);
