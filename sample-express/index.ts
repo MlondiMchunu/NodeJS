e6560ff74b58d3ef24e60f81bd0f24d447253211
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-04-21 14:14:57 +0200

    modify server

--- sample-express/index.ts
+++ sample-express/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { routeHello, routeAPINames } from "./routes.js";
 
 const app = express();
--- sample-express/routes.ts
+++ sample-express/routes.ts
@@ -1,20 +1,20 @@
 import fetch from "node-fetch";
 
 //route for /hello endpoint
-const routeHello = () => "Hello World";
+const routeHello = (): string => "Hello World";
 
 
 /*use promise based fetch to get data from external API */
 /**route for /api/names endpoint */
-const routeAPINames = async () => {
+const routeAPINames = async (): Promise<string> => {
     const url = "https://projects.propublica.org/nonprofits/api/v2/search.json?order=revenue&sort_order=desc";
-    let data;
+    let data: responseItemType[];
     try {
         const response = await fetch(url);
-        data = await response.json();
+        data = (await response.json()) as responseItemType[];
         //console.log(data);
     } catch (err) {
-        return err;
+        return "Error";
     }
     const orgs = data.organizations;
     const names = orgs
@@ -24,5 +24,7 @@ const routeAPINames = async () => {
 
     return names;
 };
+
+const routeWeather = (query: WeatherQueryInterface):WeatherDetailType 
 
 export { routeHello, routeAPINames }
