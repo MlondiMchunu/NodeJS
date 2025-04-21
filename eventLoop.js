b2d3292a864025bb6d50a6e308b4f953bbd58013
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-04-21 20:45:32 +0200

    latest commit

--- eventLoop.js
+++ eventLoop.js
@@ -11,3 +11,18 @@ function logThreeAndFour() {
 }
 
 logThreeAndFour();
+
+function longRunningTask() {
+    let count = 0;
+    for (let j = 0; j < 1e9; j++) {
+        count++;
+    }
+    console.log("Long task done!");
+}
+
+function importantTask() {
+    console.log("Important!");
+}
+
+longRunningTask();
+importantTask();
