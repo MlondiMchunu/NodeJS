1c0472495f5a63673d29116f6ffa70afaf5daa80
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-04-22 12:32:53 +0200

    add types to /hello and /api/names endpoints

--- sample-express/index.ts
+++ sample-express/index.ts
@@ -1,18 +1,18 @@
 import express, { Request, Response } from "express";
-import { routeHello, routeAPINames } from "./routes.js";
+import { routeHello, routeAPINames, routeWeather } from "./routes.js";
 
 const app = express();
 const port = 3000;
 
-app.get('/hello', async function (req, res) {
-    const response = await routeHello(req, res);
+app.get('/hello', async function (_req: Request, res: Response): Promise<void> {
+    const response = await routeHello();
     res.send(response);
 });
 
-app.get("/api/names", async function (req, res) {
+app.get("/api/names", async function (_req: Request, res: Response): Promise<void> {
     let response;
     try {
-        response = await routeAPINames(req, res);
+        response = await routeAPINames();
     } catch (err) {
         console.log(err)
     }
