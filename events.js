2dbc4e615561ee57276f9627737006f0c6d0ed1b
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-11-05 22:48:01 +0200

    add conditions to event listeners

--- events.js
+++ events.js
@@ -1,21 +1,24 @@
 const EventEmitter = require("events");
 const celebrity = new EventEmitter();
 
-
 // Subscribe to celebrity for Observer 1
-celebrity.on("race win",()=>{
-    console.log('Congratulations!')
-})
+celebrity.on("race", (result) => {
+  if (result === "win") {
+    console.log("Congratulations!", result);
+  }
+});
 
 // Subscribe to celebrity for Observer 2
-celebrity.on("race win",()=>{
-    console.log('Boo, I could have done better!')
-})
+celebrity.on("race", (result) => {
+  if (result === "lost") {
+    console.log("Boo, I could have done better!", result);
+  }
+});
 
-process.on("exit",(code)=>{
-console.log('Process exit event with code : ', code)
-})
+process.on("exit", (code) => {
+  console.log("Process exit event with code : ", code);
+});
 
-celebrity.emit("race win");
-celebrity.emit("race lost");
-celebrity.emit("race win");
+celebrity.emit("race", "win");
+celebrity.emit("race", "lost");
+//celebrity.emit("race win");
