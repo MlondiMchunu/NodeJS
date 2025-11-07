33e4dc85397872b469e65d0dc5f86755fe1460a5
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-11-07 21:46:19 +0200

    use http constant instead of request

--- http-example.js
+++ http-example.js
@@ -1,6 +1,6 @@
-const { request } = require("http");
+const http = require("http");
 
-const req = request("http://www.google.com", (res) => {
+const req = http.request("http://www.google.com", (res) => {
   res.on("data", (chunk) => {
     console.log(`Data chunk : ${chunk}`);
   });
