ab7fd4ca54fef8378ee71f66e15a813c6aed43cb
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-11-15 00:27:42 +0200

    get request

--- custom_http/https.js
+++ custom_http/https.js
@@ -1,10 +1,11 @@
 const {send} = require('./request')
 const response = require('./response')
+const {REQUEST_TIMEOUT} = require('./request')
 
 function request(url, data) {
   send(url, data);
   return response.read();
-}
+}  
 
 const responseData = request('https://wwww.google.com','hello world')
 
