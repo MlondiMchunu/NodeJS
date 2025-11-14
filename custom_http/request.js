ab11430694b0b7ee06e55cb2190c99931bae415e
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-11-15 00:27:42 +0200

    get request

--- custom_http/request.js
+++ custom_http/request.js
@@ -1,5 +1,13 @@
 const REQUEST_TIMEOUT = 500;
+const axios = require('axios');
 
+axios.get('https://dummyjson.com/carts')
+    .then(response=>{
+        console.log(response.data)
+    })
+    .catch(error=>{
+        console.error('Error fetching data:', error);
+    })
 function encrypt(data){
     return 'encrypted data'
 }
