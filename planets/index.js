726af7becf644cbb19a726cfa1b00fa2785f072d
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-11-29 23:35:40 +0200

    add pipe function to call parse function and convert bytes to csv

--- planets/index.js
+++ planets/index.js
@@ -5,13 +5,18 @@ const fs = require("fs");
 
 const results = [];
 
-fs.createReadStream("kepler_data.csv").on("data", (data) => {
-  results.push(data);
-})
-.on("error",(err)=>{
-console.log("failed to read file : ", err)
-})
-.on("end",()=>{
-  console.log(results)
-  console.log("Done!")
-})
+fs.createReadStream("kepler_data.csv")
+  .pipe(parse({
+    comment:'#',
+    columns:true,
+  }))
+  .on("data", (data) => {
+    results.push(data);
+  })
+  .on("error", (err) => {
+    console.log("failed to read file : ", err);
+  })
+  .on("end", () => {
+    console.log(results);
+    console.log("Done!");
+  });
