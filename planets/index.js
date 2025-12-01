b2f9899c0d1488f1872931de93d582197419fc59
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-12-01 23:31:59 +0200

    add habitable condition

--- planets/index.js
+++ planets/index.js
@@ -7,7 +7,8 @@ const results = [];
 
 const isHabitablePlanet = (planet) => {
   return planet["koi_disposition"] === "CONFIRMED"
-  && planet['koi_insol']> 0.36 & planet['koi_insol'] < 1.11;
+  && planet['koi_insol']> 0.36 & planet['koi_insol'] < 1.11
+  && planet['koi_prad'] < 1.6;
 };
 
 fs.createReadStream("kepler_data.csv")
