5c60fecaa881a7c948d5a00d11dddd4084566e14
Siyabonga Zungu <mlondiemchunu1@gmail.com>
2026-02-05 04:11:02 +0530

    initial sockets commit

---
+++ push-websocket/index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const { connection } = require('websocket');
+const WebsocketServer = require('websocket').server
+let connections = [];
+
+const httpserver = http.createServer()
+
+//pass httpserver object on the WebSocket library
+const websocket = new WebsocketServer({"httpServer": httpserver})
+
+//listen on the TCP socket
+httpserver.listen(8000, () => console.log("Server is listening on port 8080"))
+
+//when a legit websocket req comes, listen to it and get the connection
+websocket.on("request", request => {
+    const connection = request.accept(null, request.origin)
+    connection.on("message", message => {
+        //someone just sent a message, tell everybody
+        connections.forEach(c => c.send(`User${connection.socket.remotePort} says`))
+    })
+
+    connections.push(connection)
+    //someone just connected, tell everybody
+    connections.forEach(c => c.send(`User${connection.socket}`))
+})
+
+connections.push(connection)
+//someone just connected, tell everyone
+connections.forEach(c => c.send(`User${connection.socket.remotePort} just connected`))
