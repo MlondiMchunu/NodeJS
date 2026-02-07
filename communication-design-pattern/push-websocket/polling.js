7c5af731df74a79d083aed3b4c7f4916aca059d2
Mlondi <mlondiemchunu1@gmail.com>
2026-02-08 05:03:14 +0530

    polling

---
+++ communication-design-pattern/push-websocket/polling.js
@@ -0,0 +1,16 @@
+const app = require("express")();
+const jobs = {}
+
+app.post("/submit",(req,res)=>{
+    const jobId = `job:${Date.now()}`
+    jobs[jobId] = 0;
+    updateJob(jobId,0);
+    res.end("\n\n" + jobId + "\n\n");
+})
+
+app.get("/checkstatus",(req, res) => {
+    console.log(jobs[req.query.jobId])
+    res.end("\n\nJobStatus: " + jobs[req.query.jobId] + "%\n\n")
+})
+
+app.listen(8080, () => console.log("listening on 8080"));
